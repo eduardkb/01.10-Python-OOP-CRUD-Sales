5b0a7ed884c66b31b2fac287e9c94ed93f6b291d
eduardkb <eduardbuhali@gmail.com>
2022-06-09 20:29:04 -0300

    backend.py - update - added delete function to class Client

--- backend.py
+++ backend.py
@@ -33,31 +33,6 @@ class Client():
         database.fSql_add(Client.db_table_name, newValues)
 
     @staticmethod
-    def fUpdate_line(ID_search, dictUpdate):
-        # search for the object iwht ID x
-        cliUpdate = next(
-            (x for x in Client.all_items if x.id == ID_search), None)
-
-        # if object not found raise exception
-        if cliUpdate == None:
-            raise Exception(
-                f"Id {ID_search} to be updated on table Client not found.")
-
-        # UPDATE DATABAS
-        database.fsql_update_line(
-            Client.db_table_name, "id", ID_search, dictUpdate)
-
-        # UPDATE OBJECT
-        cliUpdate.cpf = dictUpdate["cpf"]
-        cliUpdate.name = dictUpdate["name"]
-        cliUpdate.country = dictUpdate["country"]
-        cliUpdate.city = dictUpdate["city"]
-        cliUpdate.phone = dictUpdate["phone"]
-        cliUpdate.date_nasc = dictUpdate["date_nasc"]
-
-        print(cliUpdate)
-
-    @staticmethod
     def fCreate_table():
         database.fSql_create_Table(Client.db_table_name, Client.db_fields)
 
@@ -68,6 +43,49 @@ class Client():
         for item in items:
             Client(item[0], item[1], item[2],
                    item[3], item[4], item[5], item[6], True)
+
+    @staticmethod
+    def fUpdate_line(id_search, dictUpdate):
+        # search for the object iwht ID x
+        id_search = str(id_search)
+        cliUpdate = next(
+            (x for x in Client.all_items if x.id == id_search), None)
+
+        # if object not found raise exception
+        if cliUpdate == None:
+            raise Exception(
+                f"Id {id_search} to be updated on table {Client.db_table_name} not found.")
+
+        # UPDATE DATABAS
+        database.fsql_update_line(
+            Client.db_table_name, "id", id_search, dictUpdate)
+
+        # UPDATE OBJECT
+        cliUpdate.cpf = dictUpdate["cpf"]
+        cliUpdate.name = dictUpdate["name"]
+        cliUpdate.country = dictUpdate["country"]
+        cliUpdate.city = dictUpdate["city"]
+        cliUpdate.phone = dictUpdate["phone"]
+        cliUpdate.date_nasc = dictUpdate["date_nasc"]
+
+    @staticmethod
+    def fDelete_line(id_search):
+        # search object
+        id_search = str(id_search)
+        cliDel = next(
+            (x for x in Client.all_items if x.id == id_search), None)
+        print(cliDel)
+
+        # if object not found raise exception
+        if cliDel == None:
+            raise Exception(
+                f"Id {id_search} to be deleted on table {Client.db_table_name} not found.")
+
+        # delete database item
+        database.fsql_delete_line(Client.db_table_name, "id", id_search)
+
+        # delete item from list
+        Client.all_items = [x for x in Client.all_items if x.id != id_search]
 
 
 class Retailer():
@@ -101,23 +119,26 @@ if __name__ == "__main__":
                "Ponta Grossa", 8989, "14/12/1987")
         Client(4, "444", "Siby", "Brasil",
                "Guarapuava", 9797, "29/05/1986")
-        Client(5, "555", "Helena", "Alemanha",
-               "Munich", 8880, "24/10/1959")
+        Client(5, "555", "Helena", "Alemanha", "Munich", 8880, "24/10/1959")
 
     def fTest_client_read_all():
         Client.fRead_all_from_db()
 
     def fTest_update_one():
-        id = 2
-        dictUpdate = {"cpf": 123, "name": "Laysa", "country": "ii",
-                      "city": "ii", "phone": 8080, "date_nasc": "29/12/1990"}
+        id = 3
+        dictUpdate = {"cpf": 999, "name": "Zuleima", "country": "Italy",
+                      "city": "Rome", "phone": 9999, "date_nasc": "11/11/1111"}
         a = Client.fUpdate_line(id, dictUpdate)
         print(f"Client {id} updated.")
 
+    def fTest_delete_one():
+        Client.fDelete_line(5)
+
     # fTest_client_create_table()
     # fTest_client_add()
-    fTest_print_all_objects()
+
     fTest_client_read_all()
     fTest_print_all_objects()
-    fTest_update_one()
-    fTest_print_all_objects()
+    # fTest_update_one()
+    # fTest_delete_one()
+    # fTest_print_all_objects()
